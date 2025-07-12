ad0088e3a7419fec2dcc1cadd59fe85e006d862a
Sanjiv1702 <123sanjiv2117@sjcem.edu.in>
2025-07-12 17:04:52 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir -p /var/lib/render
 
+# Set all Streamlit server configurations via CLI
 streamlit run streamlit_app.py \
   --server.port=$PORT \
   --server.headless=true \
   --server.enableCORS=false \
   --server.enableXsrfProtection=false \
-  --server.fileWatcherType=none
+  --server.fileWatcherType=none \
+  --browser.serverAddress="0.0.0.0" \
+  --browser.gatherUsageStats=false
